674a716bff13f721c675bad1ae3cfca9222c3b7b
java_learing <aswinsandeep4@gmail.com>
2024-06-13 19:39:47 +0530

    removing files from weak 1 folder to fumigation

--- weak1/technical/charater/inputchar.c
+++
@@ -1,8 +0,0 @@
-//accept char input from user and display it on the console
-#include<stdio.h>
-void main(){
-  printf("Enter a English character \n");
-  char charater;
-  scanf("%c",&charater);
-  printf("entered character is \t :%c",charater);
-}
--- weak1/technical/oddsum/sum.c
+++
@@ -1,9 +0,0 @@
-//finding the sum of odd numbers of given limit
-//sum of odd number= limit*limit
-#include<stdio.h>
-void main(){
-  int number;
-  printf("enter the limit you want to find the sum of odd number\n");
-  scanf("%d",&number);
-  printf("the sum of the number is\t  :%d",number*number);
-}
