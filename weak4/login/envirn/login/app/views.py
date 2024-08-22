5a6c0722ecdea03424b2093db096e0d3093d3058
java_learing <aswinsandeep4@gmail.com>
2024-08-22 21:04:30 +0530

    adding files

--- weak4/login/envirn/login/app/views.py
+++ weak4/login/envirn/login/app/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login
 from django.contrib.auth import logout as log
-from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_control
 from django.shortcuts import redirect
 # Create your views here.
 def index(request):
+    if 'username' in request.session:
+        return redirect(home)
     return render(request,'index.html')
 
 def logout(request):
